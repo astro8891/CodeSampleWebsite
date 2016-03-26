ca40acebc3e08779be7be08f1976f28ff431d785
Ashley <Ashley>
2016-03-26 17:56:40 +1300

    Added Jquery and correctly rendering map

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-$(function() {
-
-    function initMap() {
+//Google maps function
+//Needs to be outside of JQuery call all function as gets called from script tag in HTML
+function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: -41.221245, lng: 174.801635 },
             zoom: 11
         });
     }
+    
+//JQuery call all function
+$(function() {
+
+    
 
 });
 
