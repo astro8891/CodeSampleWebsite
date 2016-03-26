4463e3ade0a591b3c254a3136ea2c5ae343cd09d
Ashley <Ashley>
2016-03-26 17:42:25 +1300

    google maps api implimented in JS

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -41.221245, lng: 174.801635 },
+            zoom: 11
+        });
+    }
+
+});
+
